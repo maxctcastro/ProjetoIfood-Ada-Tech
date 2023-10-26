6d20cfbc286f30a466eec44f5625aca4ce6e9b4c
maxctcastro <147217612+maxctcastro@users.noreply.github.com>
2023-10-26 20:20:18 -0300

    ProjetoIfoodcompleto

---
+++ README.md
@@ -0,0 +1,12 @@
+# ProjetoIfood-ADATech
+
+####  Entrega de projeto em grupo do curso ADA Tech Front-End
+#### Modulo I - Front End Estático
+
+<br><br>
+ Integrantes: 
+ - Eric Galvão
+ - Hiago Maitan
+ - Max Castro
+ - Naira Souza
+ 
---
+++ scripts/cupons.js
@@ -0,0 +1,6 @@
+function mostrarCupom(desconto, cupom) {
+    let cupomInfo = "Aqui estão as informações do cupom:\n - Desconto: "
+    + desconto + "\n- Cupom: "+ cupom +
+    " \n- Validade: 31/12/2023";
+    window.alert(cupomInfo);
+  }
